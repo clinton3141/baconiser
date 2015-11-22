9b37ec6524d08f26838e7f613ae8273708052e7a
Clinton <iblamefish@gmail.com>
2015-11-22 21:21:48 +0000

    add missing config keys to sample config file

--- config/sample.php
+++ config/sample.php
@@ -4,7 +4,10 @@ $config = array(
   'logging' => array(
     'rollbar' => array(
       'access_token' => '',
-      'environment' => 'example_env'
+      'environment' => 'example_env',
+			'set_exception_handler' => true,
+			'set_error_handler' => true,
+			'report_fatal_errors' => true
     )
   }
 );
